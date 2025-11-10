d6f7b0c21228a8f5d92b2f6bf2722e6fc501f320
S7MS <s7ingmsk@gmail.com>
2025-11-10 13:38:07 -0300

    agregando las rutas e importaciones necesarias

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,42 @@
 import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { AuthProvider } from './context/AuthContext';
+import { CartProvider } from './context/CartContext';
+import ProductCatalogPage from './pages/ProductCatalogPage';
+import ProductDetailPage from './pages/ProductDetailPage';
+import CartPage from './pages/CartPage';
+import LoginPage from './pages/LoginPage';
+import RegisterPage from './pages/RegisterPage';
+import AdminHome from './pages/AdminHome';
+import AdminProductos from './pages/AdminProductos';
+import AdminUsuarios from './pages/AdminUsuarios';
+import NuevoProducto from './pages/NuevoProducto';
+import NuevoUsuario from './pages/NuevoUsuario';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AuthProvider>
+      <CartProvider>
+        <Router>
+          <Routes>
+            {/* rutas */}
+            <Route path="/" element={<Navigate to="/catalogo" replace />} />
+            <Route path="/catalogo" element={<ProductCatalogPage />} />
+            <Route path="/producto/:id" element={<ProductDetailPage />} />
+            <Route path="/carrito" element={<CartPage />} />
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/registro" element={<RegisterPage />} />
+            <Route path="/admin" element={<AdminHome />} />
+            <Route path="/admin/productos" element={<AdminProductos />} />
+            <Route path="/admin/usuarios" element={<AdminUsuarios />} />
+            <Route path="/admin/productos/nuevo" element={<NuevoProducto />} />
+            <Route path="/admin/usuarios/nuevo" element={<NuevoUsuario />} />
+          </Routes>
+        </Router>
+      </CartProvider>
+    </AuthProvider>
   );
 }
 
